2d4e4cc39c7f1723ffbff04e5009d8c199a6c4b8
sudhanshu-acl <sudhanshu-acl>
2026-02-27 15:24:41 +0530

    add blog detail and comment page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,15 +12,15 @@ export default async function BlogPage() {
 
   return (
     <div className="p-4">
-      <h1 className="text-3xl font-bold mb-6">Blog</h1>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <h1 className="text-3xl font-bold mb-6 text-center">Blog</h1>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 m-8">
         {posts.map((post) => (
           <Link
             key={post.id}
             href={`/blog/${post.id}`}
             className="block border rounded-lg p-4 hover:shadow-lg transition-shadow"
           >
-            <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
+            <h2 className="text-xl font-semibold mb-2 text-[#540202] h-12 leading-tight overflow-hidden">{post.title.charAt(0).toUpperCase() + post.title.slice(1)}</h2>
             <p className="text-gray-700">
               {post.body.length > 100 ? `${post.body.slice(0, 100)}...` : post.body}
             </p>
